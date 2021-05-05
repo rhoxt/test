f86e64a4a5372b204bdaddb095b89caa5bc5572d
Martin Glatzel <martin.glatzel@sap.com>
2021-05-05 09:31:04 +0200

    Fix for empty players

--- public/logic.js
+++ public/logic.js
@@ -69,6 +69,19 @@ function onFetchGames (data) {
         delete element._id;
         delete element.__v;
         element.result = element.team1score + " - " +element.team2score;
+        if(!element.team1player1) {
+            element.team1player1 = {firstName: "-", lastName: ""}
+        }
+        if(!element.team1player2) {
+            element.team1player2 = {firstName: "-", lastName: ""}
+        }
+        if(!element.team2player1) {
+            element.team2player1 = {firstName: "-", lastName: ""}
+        }
+        if(!element.team2player2) {
+            element.team2player2 = {firstName: "-", lastName: ""}
+        }
+
         element.team1player1Formatted = element.team1player1.firstName + " " + element.team1player1.lastName + " ";
         element.team1player2Formatted = element.team1player2.firstName + " " + element.team1player2.lastName + " ";
         element.team2player1Formatted = element.team2player1.firstName + " " + element.team2player1.lastName + " ";
